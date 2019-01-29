33a0c7501557d57b975af23c360f0bd78a54f4ce
Mariela Daventini <mariela.daventini@corp.themls.com>
2019-01-29 15:37:11 -0300

    full detail with -TCDetailed

--- Public/Get-TeamCityTfsChangeSet.ps1
+++ Public/Get-TeamCityTfsChangeSet.ps1
@@ -1,7 +1,7 @@
 function Get-TeamCityTfsChangeSet{
 <#
 	.SYNOPSIS
-		Get changes in TFS by version. Returns a list of projects.
+		Get changes in TFS by version. Returns a list of projects or full detail with -TCDetailed $true
 	.DESCRIPTION
 		Uses add-pssnapin Microsoft.TeamFoundation.PowerShell (Get-TfsServer and Get-TfsChangeSet)
 	.Parameter TCTFSServerUrl
@@ -10,15 +10,18 @@ function Get-TeamCityTfsChangeSet{
 		ChangeSet number
 	.Parameter TCProjectsInCI
 		Specifies an array with projects to return
+	.Parameter TCDetailed
+		Specifies if will return detailed data
 	.EXAMPLE
-		PS C:\> Get-TeamCityTfsChangeSet -TCTFSServerUrl 'http://mytfs:8080/tfs/MyCollection' -TCTFSChangeSet %build.vcs.number% -TCProjectsInCI @( 'FirstProject', 'SecondProject', 'DependantProject')
+		PS C:\> Get-TeamCityTfsChangeSet -TCTFSServerUrl 'http://mytfs:8080/tfs/MyCollection' -TCTFSChangeSet %build.vcs.number% -TCProjectsInCI @( 'FirstProject', 'SecondProject', 'DependantProject') -TCDetailed $true
 		PS C:\> Get-TeamCityTfsChangeSet -TCTFSServerUrl 'http://mytfs:8080/tfs/MyCollection' -TCTFSChangeSet 1970 -TCProjectsInCI @( 'FirstProject', 'SecondProject', 'DependantProject')
 #>
 	[CmdletBinding()]
 	param(
 		[parameter(HelpMessage="Must be a valid Tfs Url. Example -TCTFSServerUrl 'TFS 2010+: http[s]://<TFS Server>:<Port>/tfs/<Collection Name>'")][ValidateNotNullOrEmpty()][String[]]$TCTFSServerUrl, 
-		[parameter(HelpMessage="ChangeSet number. Example -TCTFSChangeSet 1970")][ValidateNotNullOrEmpty()][String[]]$TCTFSChangeSet,
-		[parameter(HelpMessage="Array of TFS Project Names. Example -TCProjectsInCI @( 'FirstProject', 'SecondProject', 'DependantProject')")][ValidateNotNullOrEmpty()][String[]]$TCProjectsInCI
+		[parameter(HelpMessage="ChangeSet number. Example -TCTFSChangeSet 1970")][ValidateNotNullOrEmpty()][Int]$TCTFSChangeSet,
+		[parameter(HelpMessage="Array of TFS Project Names. Example -TCProjectsInCI @( 'FirstProject', 'SecondProject', 'DependantProject')")][ValidateNotNullOrEmpty()][String[]]$TCProjectsInCI,
+		[parameter(HelpMessage="Use -TCDetailed to get full detail of files in commit. Example -TCDetailed")][Switch]$TCDetailed
 	)
 	Write-Verbose "Get-TeamCityTfsChangeSet"
 	add-pssnapin Microsoft.TeamFoundation.PowerShell
@@ -29,17 +32,21 @@ function Get-TeamCityTfsChangeSet{
 	try {
 		Write-Verbose "Getting TFSChanges using -TCTFSServerUrl $TCTFSServerUrl -TCTFSChangeSet $TCTFSChangeSet"
 		$ProjectsInCommit = @()
-		$ThisTFSServer = Get-TfsServer -name $TCTFSServerUrl -Credential $TCCredential -Verbose:$Verbose
+		$ThisTFSServer = Get-TfsServer -name "$TCTFSServerUrl" -Credential $TCCredential -Verbose:$Verbose
 		$ThisChangeSet = Get-TfsChangeSet -ChangesetNumber $TCTFSChangeSet -Server $ThisTFSServer -Verbose:$Verbose
 		ForEach ($ThisChange in $ThisChangeSet.Changes) {
 			$ThisFile = ($ThisChange.Item.ServerItem).Remove(0,2)
 			$ThisProjectEndIndex = $ThisFile.IndexOf('/')
 			$ThisProject = $ThisFile.Substring(0, $ThisProjectEndIndex)
-			if ($TCProjectsInCI -contains $ThisProject) {
+			if ( ( $TCProjectsInCI -contains $ThisProject ) -or ( -not $TCProjectsInCI ) ) {
 				if ($ProjectsInCommit -notcontains $ThisProject) {
 					$ProjectsInCommit += $ThisProject
 				}
 			}
+		}
+		If ( $TCDetailed ) {
+			Write-Verbose -Message "Will return Details In Commit" 
+			$ProjectsInCommit = $ThisChangeSet | Select-Object -Property Committer, Comment -ExpandProperty Changes | Select-Object -Property Committer, Comment -ExpandProperty Item | Select-Object -Property Committer, Comment, ServerItem 
 		}
 		$TCOutput = $ProjectsInCommit | out-string
 		Write-Verbose -Message "ProjectsInCommit $TCOutput" 
